0aae8391cb4106d5e272bb13e739ffae53f7726b
maruthimahesh <maruthimahesh373@gmail.com>
2023-08-02 03:23:49 +0530

    MMS

--- selenium/src/java_30/Binary_search.java
+++ selenium/src/java_30/Binary_search.java
@@ -1,3 +1,4 @@
+
 package java_30;
 
 import java.util.Arrays;
--- selenium/src/java_30/Reverse_a_String.java
+++ selenium/src/java_30/Reverse_a_String.java
@@ -6,39 +6,40 @@ public class Reverse_a_String {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner sc=new Scanner(System.in);
-System.out.println("enter the String");
-String rev="";
-/*
- * { String str=sc.next(); int len=str.length(); for(int i=len-1;i>=0;i--) {
- * rev=rev+str.charAt(i);
- * 
- * 
- * 
- * } System.out.println(rev); }
- */
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the String");
+		String input = sc.nextLine(); // Use nextLine() instead of next() and store the input in a single variable
 
-//using the sting buffer
-{
-	String S1=sc.next();
-	StringBuffer S1=new StringBuffer(S1);
-StringBuffer rev1=S11.reverse();
-System.out.println(rev1);
-}
-//using teh char at array
-{
-	System.out.println("enter the String");
+		// using charAt method
+		{
+			String rev = ""; // Initialize rev inside each block
+			int len = input.length();
+			for (int i = len - 1; i >= 0; i--) {
+				rev = rev + input.charAt(i);
+			}
+			System.out.println("Reversed string using charAt method: " + rev);
+		}
 
-	String S=sc.next();
-
-	char a[]=S.toCharArray();
-	int len=a.length;
-	for(int i=len-1;i>=0;i--) {
-		rev=rev+a[i];
-}
-
+		// using StringBuffer
+		{
+			StringBuffer input1 = new StringBuffer(input);
+			StringBuffer rev1 = input1.reverse();
+			System.out.println("Reversed string using StringBuffer: " + rev1);
+		}
+		
+		// using char array
+		{
+			String rev = ""; // Initialize rev inside each block
+			char a[] = input.toCharArray();
+			int len = a.length;
+			for (int i = len - 1; i >= 0; i--) {
+				rev = rev + a[i];
+			}
+			System.out.println("Reversed string using char array: " + rev);
+		}
+		
+		sc.close(); // Close the scanner object
 
 	}
 
-}
 }
